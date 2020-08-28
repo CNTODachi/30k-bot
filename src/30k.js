075adb0af9d33e93043375eada5a0ce6ebb37a82
CNTODachi <dene_ford@yahoo.com>
2020-08-28 20:21:56 +0100

    New titanicus role added

--- src/30k.js
+++ src/30k.js
@@ -27,6 +27,7 @@ const Wolves = '697856557664305273';
 const Auxilia = '698095353492406343';
 const Excertus = '698143883439374356';
 const Dae = '698459450247217193';
+const tit = '748978438261702738';
 
 
 client.on('ready', () => {
@@ -50,7 +51,8 @@ iron hands              sons of horus           alpha legion
 ultramarines            blood angels            imperial fists
 emperors children       white scars             space wolves
 custodes                sisters of silence      mechanicum
-imperialis auxilia      excertus imperialis     daemons`);
+imperialis auxilia      excertus imperialis     daemons
+legio titanicus`);
 
 });
 
@@ -225,6 +227,13 @@ client.on('message', function(message) {
         message.member.roles.add(Dae);
     }
 
+    else if(message.content === 'legio titanicus') {
+        const user = message.author;
+        let arrivalswelcome = client.channels.cache.get('697220115774701660');
+        arrivalswelcome.send(`<@${user.id}> We are all but weapons in the right hand of the Emperor! Use <#697008604162687028> to say hello and welcome my friends.`);
+        message.member.roles.add(tit);
+    }
+
     else if(message.content === 'remove night lords') {
         const user = message.author;
         let arrivalswelcome = client.channels.cache.get('697220115774701660');
@@ -391,6 +400,13 @@ client.on('message', function(message) {
         let arrivalswelcome = client.channels.cache.get('697220115774701660');
         arrivalswelcome.send(`<@${user.id}> Role removed.`);
         message.member.roles.remove(Dae);
+    }
+
+    else if(message.content === 'remove legio titanicus') {
+        const user = message.author;
+        let arrivalswelcome = client.channels.cache.get('697220115774701660');
+        arrivalswelcome.send(`<@${user.id}> Role removed.`);
+        message.member.roles.remove(tit);
     }
 
 });
